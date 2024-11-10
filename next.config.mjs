baa84cc4ae203642b26c858ef11fba17bc865314
paul bartolo <bartolopaul11@gmail.com>
2024-11-11 03:09:59 +0800

    fixing production env and other issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
   };
   
   export default nextConfig;
